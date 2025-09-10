b1ba5df253e44d729dcf1ce98b9172289c79e3f1
tejas7410 <tejaskangule7410509433@gmail.com>
2025-09-11 05:05:18 +0530

    update2

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Video1 from '../assets/Video1.mp4';
-import Video2 from '../assets/Video2.mp4';
+import Video2 from '../assets/V.mp4';
 import Video3 from '../assets/Video3.mp4';
 import Video4 from '../assets/Video4.mp4';
 
